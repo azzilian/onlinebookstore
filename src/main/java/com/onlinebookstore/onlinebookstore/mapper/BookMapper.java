a5f6b64325416e52f30fea76f86cd2425a6655f9
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-04-26 14:39:36 +0300

    removed id from CreateBookRequestDto

--- src/main/java/com/onlinebookstore/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/mapper/BookMapper.java
@@ -5,10 +5,12 @@ import com.onlinebookstore.onlinebookstore.dto.BookDto;
 import com.onlinebookstore.onlinebookstore.dto.CreateBookRequestDto;
 import com.onlinebookstore.onlinebookstore.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
-    BookDto toDto(Book book);
-
+    @Mapping(target = "id", ignore = true)
     Book toModel(CreateBookRequestDto requestDto);
+
+    BookDto toDto(Book book);
 }
