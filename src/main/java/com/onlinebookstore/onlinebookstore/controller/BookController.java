78f8dd220c059832fe88a4e7772c511c920fc6d4
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-04-29 17:21:06 +0300

    minor fixes

--- src/main/java/com/onlinebookstore/onlinebookstore/controller/BookController.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/controller/BookController.java
@@ -3,12 +3,19 @@ package com.onlinebookstore.onlinebookstore.controller;
 import com.onlinebookstore.onlinebookstore.dto.BookDto;
 import com.onlinebookstore.onlinebookstore.dto.CreateBookRequestDto;
 import com.onlinebookstore.onlinebookstore.dto.UpdateBookRequestDto;
-import com.onlinebookstore.onlinebookstore.exeption.DataProcessingException;
 import com.onlinebookstore.onlinebookstore.service.BookService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
--- src/main/java/com/onlinebookstore/onlinebookstore/service/BookService.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/service/BookService.java
@@ -3,12 +3,13 @@ package com.onlinebookstore.onlinebookstore.service;
 import com.onlinebookstore.onlinebookstore.dto.BookDto;
 import com.onlinebookstore.onlinebookstore.dto.CreateBookRequestDto;
 import com.onlinebookstore.onlinebookstore.dto.UpdateBookRequestDto;
-
 import java.util.List;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
+
     BookDto update(Long id, UpdateBookRequestDto requestDto);
+
     BookDto delete(Long id);
 
     List<BookDto> findAll();
