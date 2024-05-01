cb59d6b7ee6ab025abb4b3875c83941e42852a7a
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-01 17:50:17 +0300

    validation updated

--- src/main/java/com/onlinebookstore/onlinebookstore/dto/BookRequestDto.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/dto/BookRequestDto.java
@@ -2,23 +2,27 @@ package com.onlinebookstore.onlinebookstore.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class BookRequestDto {
-    @NotNull
-    @Size(min = 1)
+    @NotNull(message = "title field cannot be empty")
+    @Size(min = 1, message = "title name should be at least 1 character")
     private String title;
-    @NotNull
-    @Size(min = 1)
+    @NotNull(message = "author field cannot be empty")
+    @Size(min = 1, message = "author name should be at least 1 character")
     private String author;
-    @NotNull
-    @Size(min = 1)
+    @NotNull(message = "ISBN field cannot be empty")
+    @Pattern(regexp = "^(?:ISBN(?:-13)?:? )"
+            + "?(?=[-0-9]{17}$|[-0-9X]{13}$|[-0-9]{10}$|(?=(?:[-0-9]{2,6})"
+            + "?[-0-9]{1,5}-|)[- 0-9X]{1,17}$)97[89][- ]?[0-9]+[- ]"
+            + "?[0-9]+[- ]?[0-9]+[- ]?[0-9]$", message = "Invalid ISBN format, tip use ISBN-13")
     private String isbn;
-    @NotNull
-    @Min(0)
+    @NotNull(message = "price field cannot be empty")
+    @Min(value = 0, message = "price cannot be lover than 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
