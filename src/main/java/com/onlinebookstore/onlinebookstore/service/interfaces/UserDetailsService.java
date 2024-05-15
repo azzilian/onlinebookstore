d26aa59e2b0bc3c7ead742ddd8d666c245a45fa1
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-15 10:43:09 +0300

    UserDetailsService.java no longer used

--- src/main/java/com/onlinebookstore/onlinebookstore/service/interfaces/UserDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.onlinebookstore.onlinebookstore.service.interfaces;
-
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-public interface UserDetailsService {
-    UserDetails loadUserByUsername(String name) throws UsernameNotFoundException;
-}
