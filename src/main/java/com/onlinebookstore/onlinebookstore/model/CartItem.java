f0045abfe8e5f82ea65534ac77349512a881c4d4
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-21 00:40:59 +0300

    Created Entities Shopping cart and cart items + liquibase

---
+++ src/main/java/com/onlinebookstore/onlinebookstore/model/CartItem.java
@@ -0,0 +1,33 @@
+package com.onlinebookstore.onlinebookstore.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "cartitems")
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @ManyToOne
+    @JoinColumn(name = "shoppingcart_id")
+    private ShoppingCart shoppingCart;
+    @NotBlank
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    @NotBlank
+    private int quantity;
+
+}
---
+++ src/main/java/com/onlinebookstore/onlinebookstore/model/ShoppingCart.java
@@ -0,0 +1,32 @@
+package com.onlinebookstore.onlinebookstore.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "shopingcarts")
+@Getter
+@Setter
+public class ShoppingCart {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+    @NotBlank
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<CartItem> cartItems;
+}
