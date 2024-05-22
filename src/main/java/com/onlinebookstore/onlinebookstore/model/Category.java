208fe0630e9cb4cecabe9587cdd1fb655bddee5e
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-22 05:10:56 +0300

    Removed many to many from categories

--- src/main/java/com/onlinebookstore/onlinebookstore/model/Category.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/model/Category.java
@@ -2,15 +2,11 @@ package com.onlinebookstore.onlinebookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import java.util.HashSet;
-import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
@@ -35,6 +31,4 @@ public class Category {
 
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted = false;
-    @ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY)
-    private Set<Book> books = new HashSet<>();
 }
