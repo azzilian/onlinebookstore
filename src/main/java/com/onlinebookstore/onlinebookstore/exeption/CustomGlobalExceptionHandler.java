69c3a0dedb8be84542ab1838b18e8bfbd4119a6a
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-22 06:16:16 +0300

    exception fix

--- src/main/java/com/onlinebookstore/onlinebookstore/exeption/CustomGlobalExceptionHandler.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/exeption/CustomGlobalExceptionHandler.java
@@ -49,13 +49,14 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(InvalidCategoryIdException.class)
     public ResponseEntity<Object> handleInvalidCategoryIdException(
             InvalidCategoryIdException ex, WebRequest request) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpStatus.BAD_REQUEST.value());
         body.put("error", ex.getMessage());
-        body.put("path", request.getDescription(false));
+        body.put("path", request.getContextPath());
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
