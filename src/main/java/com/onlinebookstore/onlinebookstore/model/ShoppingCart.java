60af436dc1176062a49e29d3151ae894c8625736
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-23 01:57:28 +0300

    fixes according to comments

--- src/main/java/com/onlinebookstore/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/model/ShoppingCart.java
@@ -1,6 +1,5 @@
 package com.onlinebookstore.onlinebookstore.model;
 
-//import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
