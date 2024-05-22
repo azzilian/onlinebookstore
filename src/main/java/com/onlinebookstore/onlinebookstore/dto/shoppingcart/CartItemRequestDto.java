f5186327c79d2d563f39948dddb2650e56714a83
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-22 03:39:00 +0300

    Validation request added

--- src/main/java/com/onlinebookstore/onlinebookstore/dto/shoppingcart/CartItemRequestDto.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/dto/shoppingcart/CartItemRequestDto.java
@@ -1,9 +1,15 @@
 package com.onlinebookstore.onlinebookstore.dto.shoppingcart;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class CartItemRequestDto {
+    @Min(value = 1, message = "minimal quantity is at least 1")
     private Long bookId;
+    @NotNull(message = "quantity field cannot be empty")
+    @Min(value = 1, message = "minimal quantity is at least 1")
     private int quantity;
 }
