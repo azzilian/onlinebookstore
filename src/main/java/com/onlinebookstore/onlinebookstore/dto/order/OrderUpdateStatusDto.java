aad83bcc8c20be7171a17066e9fbaf5c5bde27e3
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-24 01:25:55 +0300

    patch fixed

--- src/main/java/com/onlinebookstore/onlinebookstore/dto/order/OrderUpdateStatusDto.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/dto/order/OrderUpdateStatusDto.java
@@ -3,9 +3,10 @@ package com.onlinebookstore.onlinebookstore.dto.order;
 import com.onlinebookstore.onlinebookstore.model.orders.OrderStatus;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 public class OrderUpdateStatusDto {
-    @NotNull
     private OrderStatus orderStatus;
 }
