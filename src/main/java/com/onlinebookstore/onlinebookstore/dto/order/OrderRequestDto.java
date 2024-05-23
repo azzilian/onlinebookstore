7095a7f8b06a378157a89ba63c512e86a3791993
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-23 13:29:03 +0300

    service + endpoints added -raw version

--- src/main/java/com/onlinebookstore/onlinebookstore/dto/order/OrderRequestDto.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/dto/order/OrderRequestDto.java
@@ -1,12 +1,19 @@
 package com.onlinebookstore.onlinebookstore.dto.order;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
 import lombok.Data;
 
 @Data
 public class OrderRequestDto {
-    @NotNull(message = "shippingAddress field cannot be empty")
+    private Long userId;
+    @NotBlank(message = "shippingAddress field cannot be empty")
     @Size(min = 1, message = "shippingAddress should be at least 1 character")
     private String shippingAddress;
+    private BigDecimal total;
+    private LocalDateTime orderDate;
+    private Set<OrderItemRequestDto> orderItemDto;
 }
