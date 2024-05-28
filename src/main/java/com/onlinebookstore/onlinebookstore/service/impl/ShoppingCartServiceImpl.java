64326426e4c9a8995622e7143386337d52f4f2d6
Vladyslav Vyshynskyi <crusotdeath@gmail.com>
2024-05-28 13:42:58 +0300

    fixes according to comments

--- src/main/java/com/onlinebookstore/onlinebookstore/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/com/onlinebookstore/onlinebookstore/service/impl/ShoppingCartServiceImpl.java
@@ -18,8 +18,10 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ShoppingCartServiceImpl implements ShoppingCartService {
     private static final Logger logger = LoggerFactory.getLogger(ShoppingCartServiceImpl.class);
